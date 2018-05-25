8ef6ba6c7f8515af3723ddeac2ea192a374380ae
Lyle Rosdahl <lyle.rosdahl@gmail.com>
2018-05-24 21:44:36 -0500

    Change function.

--- greeting.js
+++ greeting.js
@@ -1,5 +1,9 @@
 // Show a greeting for day of the week.
 
-let greeting = date => {
-		return `Hope you are having a great ${day(date)}.`;
-};
+function greeting(date) {
+		return day(date);
+}
+
+//let greeting = date => {
+//		return `Hope you are having a great ${day(date)}.`;
+//};
