ca4563cd6e63d94202435f518e4d4f468a3c97cd
Lyle Rosdahl <lyle.rosdahl@gmail.com>
2018-05-24 21:11:50 -0500

    Cut function add to new file day.js.

---
+++ day.js
@@ -0,0 +1,6 @@
+// Returns the day of the week for the given date.
+
+let day = date => {
+		const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		return dayOfTheWeek[date.getDay()];
+}
