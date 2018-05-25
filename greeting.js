f82afe0ef84c79d232d3cf7c107979a949f63364
Lyle Rosdahl <lyle.rosdahl@gmail.com>
2018-05-25 17:17:08 -0500

    Refactor Greeting function. Move .txt to folder.

--- greeting.js
+++ greeting.js
@@ -1,9 +1,9 @@
 // Show a greeting for day of the week.
 
-function greeting(date) {
-		return `Hope you're having a great ${day(date)}.`;
-}
+//function greeting(date) {
+//		return `Hope you're having a great ${day(date)}.`;
+//}
 
-//let greeting = date => {
-//		return `Hope you are having a great ${day(date)}.`;
-//};
+let greeting = date => {
+		return `Hope you are having a great ${day(date)}.`;
+}
