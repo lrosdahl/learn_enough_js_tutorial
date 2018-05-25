9b271333aea1ea167c9e835db787abeac4f4e922
Lyle Rosdahl <lyle.rosdahl@gmail.com>
2018-05-25 17:31:07 -0500

    Add file.

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+// Function to find if a word or phrase is a palindrome
+
+let palindrome = string => {
+		string === reverse(string);
+}
+
+let reverse = string => {
+		return string.toLowerCase.split("").reverse().join("");
+}
