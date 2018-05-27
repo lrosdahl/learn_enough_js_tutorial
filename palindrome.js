c7d64e7448d24d06e33f8ece7bdb1a017df1341e
Lyle Rosdahl <lyle.rosdahl@gmail.com>
2018-05-27 15:26:54 -0500

    Fix Object Prototype issues.

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,49 @@
 // Function to find if a word or phrase is a palindrome
 
-let palindrome = string => {
-		return string === reverse(string);
-}
-
-let reverse = string => {
-		return string.toLowerCase().split("").reverse().join("");
+// Reverses a string.
+function reverse(string) {
+		return Array.from(string).reverse().join("");
 }
 
 function Phrase(content) {
 		this.content = content;
+
+		// Return a lowercase string
+		this.processor = function(string) {
+				return string.toLowerCase();
+		}
+
+		// Return processed content
+		this.processedContent = function() {
+				return this.processor(this.content);
+		}
+
+		// Returns true for a palindrome, false otherwise.
+		this.palindrome = function palindrome() {
+				return this.processedContent() === reverse(this.processedContent());
+		}
 }
+
+function TranslatedPhrase(content, translation) {
+		this.content = content;
+		this.translation = translation;
+
+		// Returns true for a palindrome, false otherwise.
+		this.processedContent = function() {
+				return this.processor(this.translation);
+		}
+}
+
+
+TranslatedPhrase.prototype = new Phrase();
+
+// *********************************
+
+// fat arrow functions
+//let palindrome = string => {
+//		return string === reverse(string);
+//}
+//
+//let reverse = string => {
+//		return string.toLowerCase().split("").reverse().join("");
+//}
